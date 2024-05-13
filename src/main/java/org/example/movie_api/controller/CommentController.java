16cddf12f5df12fc2e4f43b0690cbf39d73c0033
DrugCoding <lhs940308@gmail.com>
2024-05-13 12:21:01 +0900

    comment detail

--- src/main/java/org/example/movie_api/controller/CommentController.java
+++ src/main/java/org/example/movie_api/controller/CommentController.java
@@ -1,10 +1,14 @@
 package org.example.movie_api.controller;
 
 
+import org.example.movie_api.dto.CommentDto;
+import org.example.movie_api.entity.Comment;
 import org.example.movie_api.service.CommentService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.tokens.CommentToken;
 
 @RestController
+@RequestMapping("/comment")
 public class CommentController {
 
     private final CommentService commentService;
@@ -12,4 +16,19 @@ public class CommentController {
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
     }
+
+    // 댓글 작성
+    @PostMapping("/create")
+    public Comment createComment(@RequestBody CommentDto commentDto) {
+
+        return commentService.saveComment(commentDto);
+
+    }
+
+    // 댓글 확인 id로
+    @GetMapping("/detail/{id}")
+    public Comment commentDetail(@PathVariable Long id) {
+        return commentService.detailOne(id);
+    }
+
 }
