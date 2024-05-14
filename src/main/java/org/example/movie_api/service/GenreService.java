475f6e4465dec12ad6676078e986b6d58c03aece
DrugCoding <lhs940308@gmail.com>
2024-05-14 16:50:07 +0900

    GenreService 코드수정(통일)

--- src/main/java/org/example/movie_api/service/GenreService.java
+++ src/main/java/org/example/movie_api/service/GenreService.java
@@ -5,8 +5,10 @@ import org.example.movie_api.dto.GenreDto;
 import org.example.movie_api.dto.GenreListResponse;
 import org.example.movie_api.entity.Genre;
 import org.example.movie_api.repository.GenreRepository;
+import org.hibernate.query.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -24,26 +26,54 @@ public class GenreService {
         this.genreRepository = genreRepository;
     }
 
-//    private WebClient webClient = WebClient.create("https://api.themoviedb.org/3");
-
     public String saveGenre() {
-        List<GenreDto> genres = webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/genre/movie/list")
-                        .queryParam("language", "ko")
-                        .build())
-                .retrieve()
-                .bodyToMono(GenreListResponse.class)
-                .block()
-                .getGenres();
+        // 여러 데이터를 비동기적으로 저장하고 처리할 때 사용
+        // Flux.range() 로 시작. 한 페이지에 다 있기에 1,1
+        Flux
+                .range(1, 1)
+                .flatMap(page -> webClient
+                        .get()
+                        .uri(uriBuilder -> uriBuilder.path("/genre/movie/list")
+                                .queryParam("language", "ko")
+                                .queryParam("page", page)
+                                .build())
+                        .retrieve()
+                        .bodyToMono(GenreListResponse.class)
+                        .map(GenreListResponse::getGenres))
+                .flatMap(Flux::fromIterable)
+                .map(genreDto -> {
+                    Genre genre = new Genre();
+                    genre.setId(genreDto.getId());
+                    genre.setName(genreDto.getName());
+                    return genre;
+                })
+                .collectList()
+                .doOnNext(genreRepository::saveAll)
+                .block();
 
-        for (GenreDto genre : genres) {
-            Genre genre1 = new Genre();
-            genre1.setId(genre.getId());
-            genre1.setName(genre.getName());
-            genreRepository.save(genre1);
-        }
-
-        return "genre 데이터 저장완료";
+        return "장르 데이터 저장 완료";
 
     }
 }
+
+// 변경 전 코드
+//        List<GenreDto> genres = webClient.get()
+//                .uri(uriBuilder -> uriBuilder.path("/genre/movie/list")
+//                        .queryParam("language", "ko")
+//                        .build())
+//                .retrieve()
+//                .bodyToMono(GenreListResponse.class)
+//                .block()
+//                .getGenres();
+//
+//        for (GenreDto genre : genres) {
+//            Genre genre1 = new Genre();
+//            genre1.setId(genre.getId());
+//            genre1.setName(genre.getName());
+//            genreRepository.save(genre1);
+//        }
+//
+//        return "genre 데이터 저장완료";
+//
+//    }
+//}
